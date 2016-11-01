e37c34234e724ddea7800d7483f1ebd786d459cb
Alexander <Alexander>
2016-11-01 17:16:51 +0300

    Задача решена

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,19 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as data_file:
+        return data_file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    refined_text = re.findall(r'[^ \W|\d]+', text)
+    counter = Counter(refined_text)
+    return counter.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    data = (load_data(input('Введите путь до файла: ')))
+    data = get_most_frequent_words(data)
+    print(data)
