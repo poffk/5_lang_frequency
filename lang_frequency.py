e73c08f0689be016d3fb6d3af5af6786872a347b
Alexander <Alexander>
2016-11-22 17:54:52 +0300

    Ошибки исправлены

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,15 +2,19 @@ import re
 from collections import Counter
 
 
+NUMBER_OF_OUTPUT_WORDS = 10
+
+
 def load_data(filepath):
     with open(filepath) as data_file:
         return data_file.read()
 
 
 def get_most_frequent_words(text):
+    text.lower()
     refined_text = re.findall(r'[^ \W|\d]+', text)
     counter = Counter(refined_text)
-    return counter.most_common(10)
+    return counter.most_common(NUMBER_OF_OUTPUT_WORDS)
 
 
 if __name__ == '__main__':
